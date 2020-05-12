52feee3608d8514e7ba4efa55e8257ce4e630a15
Oleg Gelfand <oleggelfand.1@gmail.com>
2020-05-12 15:04:18 -0400

    minor connection change

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ let mongoURI = ""
 if (process.env.NODE_ENV === "production") {
     mongoURI = process.env.DB_URL;
   } else {
-    mongoURI = "mongodb://localhost/RoastersCorner";
+    mongoURI = 'mongodb://localhost/RoastersCorner';
   }
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -12,3 +12,6 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 module.exports = mongoose
+
+
+// heroku config.set DB_URL="mongodb+srv://oleg123:oleg123@roasterscorner-d9rgo.mongodb.net/test?retryWrites=true&w=majority"
