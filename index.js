49f7b506e55cc9bc2b021039d65f4c66efa126b1
Oleg Gelfand <oleggelfand.1@gmail.com>
2020-05-12 13:45:18 -0400

    ..

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express")
 const parser = require("body-parser")
 const cors = require("cors")
 const app = express()
-const connectDB = require('./config/db');
+// const connectDB = require('./config/db');
 
-connectDB();
+// connectDB();
 app.use(cors())
 app.use(parser.urlencoded({ extended: true }))
 app.use(parser.json())
@@ -14,29 +14,19 @@ app.use(express.json({extended: false}));
 
 
 
-
-
-
-// app.set("port", process.env.PORT || 8080)
-
-// app.listen(app.get("port"), () => {
-//     console.log(`✅ PORT: ${app.get("port")} 🌟`)
-// })
-
-// Haleys
-
-
-app.use(express.json({ extended: false }));
-
 const roasterDataRouter = require("./routes/roasterDataRouter")
 app.use('/roasters', roasterDataRouter)
 
 const coffeeDataRouter = require("./routes/coffeeDataRouter")
 app.use('/coffees', coffeeDataRouter)
-app.use("/users", require("./routes/users"));
-app.use("/auth", require("./routes/auth"));
 
+// app.use('/users', require('.routes/users'))
+app.set("port", process.env.PORT || 8080)
 
+app.listen(app.get("port"), () => {
+    console.log(`✅ PORT: ${app.get("port")} 🌟`)
+})
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
