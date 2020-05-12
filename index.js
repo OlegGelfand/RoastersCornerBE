9b90bd4e3406216cf9fdbc8eb597d5fafa639ff7
Oleg Gelfand <oleggelfand.1@gmail.com>
2020-05-12 13:33:17 -0400

    trying to deploy

--- index.js
+++ index.js
@@ -12,18 +12,31 @@ app.use(express.json({extended: false}));
 // const coffeeDataRouter = require("./routes/coffeeDataRouter")
 // app.use('/coffees', coffeeDataRouter)
 
-const roasterDataRouter = require("./routes/roasterDataRouter")
-app.use('/roasters', roasterDataRouter)
 
-const coffeeDataRouter = require("./routes/coffeeDataRouter")
-app.use('/coffees', coffeeDataRouter)
 
-// app.use('/users', require('.routes/users'))
+
+
+
 // app.set("port", process.env.PORT || 8080)
 
 // app.listen(app.get("port"), () => {
 //     console.log(`✅ PORT: ${app.get("port")} 🌟`)
 // })
+
+// Haleys
+
+
+app.use(express.json({ extended: false }));
+
+const roasterDataRouter = require("./routes/roasterDataRouter")
+app.use('/roasters', roasterDataRouter)
+
+const coffeeDataRouter = require("./routes/coffeeDataRouter")
+app.use('/coffees', coffeeDataRouter)
+app.use("/users", require("./routes/users"));
+app.use("/auth", require("./routes/auth"));
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
