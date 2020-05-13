81fecf0123ec4b356eb1894df6108851d2a5974b
Oleg Gelfand <oleggelfand.1@gmail.com>
2020-05-13 19:00:25 -0400

    more coffee data

--- models/Coffee.js
+++ models/Coffee.js
@@ -2,10 +2,14 @@ const mongoose = require('../db/connection')
 const Schema = mongoose.Schema
 
 const CoffeeSchema = new Schema({
+    RoasterName:String,
     coffeeName: String,
+    roastType: ["light", "Medium", "Dark", "Ga"],
+    availableIn:["8oz","12oz","16oz", "Custom"],
     originDate: { type: String, default: new Date() },
     body: String,
-    numLikes: String,  
+    numOrders: { type: Number, default: 0 },
+   
 })
 
 const Coffee = mongoose.model('Coffee', CoffeeSchema)
